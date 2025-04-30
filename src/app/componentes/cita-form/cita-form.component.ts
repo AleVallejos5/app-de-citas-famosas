a719dc3640eb949b24673ec3edcd99332eafbcb2
AleVallejos5 <avallejosahumada@gmail.com>
2025-04-30 06:07:21 -0400

    cita-form para agregar los campos del formulario y las validaciones

--- src/app/componentes/cita-form/cita-form.component.ts
+++ src/app/componentes/cita-form/cita-form.component.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { IonInput, IonCardTitle, IonCardHeader, IonCard, IonItem, IonButton, IonNote } from "@ionic/angular/standalone";
+import { IonInput, IonCardTitle, IonCardHeader, IonCard,
+   IonItem, IonButton, IonNote } from "@ionic/angular/standalone";
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cita-form',
   templateUrl: './cita-form.component.html',
   styleUrls: ['./cita-form.component.scss'],
-  imports: [IonNote, IonButton, IonItem, IonButton, IonInput, IonCardTitle, IonCardHeader, IonCard, FormsModule],
+  imports: [IonNote, IonButton, IonItem, IonButton, IonInput,
+     IonCardTitle, IonCardHeader, IonCard, FormsModule, CommonModule],
   standalone: true,
 })
 export class CitaFormComponent  implements OnInit {
   textoStr: string = "";
   autorStr: string = "";
 
-  @Output() onCreate = new EventEmitter<{ texto: string; autor: string }>()
+  @Output() onCreate = new EventEmitter<{ texto: string; autor: string }>();
 
   constructor() { }
 
   ngOnInit() {}
 
   onClick() {
-    console.log("producto::onClick")
-    this.onCreate.emit({ texto: this.textoStr, autor: this.autorStr });
-    this.textoStr = '';
-    this.autorStr = '';
+    if (this.textoStr && this.autorStr) {
+      this.onCreate.emit({
+        texto: this.textoStr.trim(),
+        autor: this.autorStr.trim()
+      });
+      this.textoStr = '';
+      this.autorStr = '';
+    }
   }
-
 }
