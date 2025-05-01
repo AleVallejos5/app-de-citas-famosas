d2567f5a446b9b4b9f3dda1a04e83f62cc1d08fb
AleVallejos5 <avallejosahumada@gmail.com>
2025-05-01 00:23:03 -0400

    copiar carpeta de sql.js a assent y modificar l main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+import { defineCustomElements } from 'jeep-sqlite/loader'
+
+if (environment.production) {
+  enableProdMode();
+}
+
+defineCustomElements(window)
 
 bootstrapApplication(AppComponent, {
   providers: [
