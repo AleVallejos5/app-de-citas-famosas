2be88bc6337529f959ea137a93e2206013dbc2ba
AleVallejos5 <avallejosahumada@gmail.com>
2025-05-07 01:45:10 -0400

    modificado con estilo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonIcon, IonButton, IonCard, IonCardContent} from '@ionic/angular/standalone';
-import { buildOutline, refreshOutline, settingsOutline, trashOutline } from 'ionicons/icons';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonIcon, IonButton, IonCard, IonCardContent, IonFab } from '@ionic/angular/standalone';
+import { buildOutline, refreshOutline, settingsOutline, trashOutline, add } from 'ionicons/icons';
 import { Router, RouterModule } from '@angular/router';
 import { Cita } from 'src/app/modelo/cita';
 import { CitasService } from 'src/app/servicios/citas.service';
@@ -12,7 +12,7 @@ import { ConfiguracionService } from 'src/app/servicios/configuracion.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonCardContent, IonCard, IonButton,
+  imports: [IonFab, IonCardContent, IonCard, IonButton,
      IonIcon, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, RouterModule],
 })
 export class HomePage implements OnInit {
@@ -25,7 +25,7 @@ export class HomePage implements OnInit {
     private configService: ConfiguracionService
   ) {
 
-    addIcons({ buildOutline, settingsOutline, refreshOutline, trashOutline});
+    addIcons({settingsOutline,trashOutline,add,buildOutline,refreshOutline});
   }
 
   async ngOnInit() {
